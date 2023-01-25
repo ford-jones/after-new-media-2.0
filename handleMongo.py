901974599616f5743a7c564ae60afb7ffbea58e2
ford-jones <fordnicholasjones@gmail.com>
2023-01-25 22:31:09 +1300

    succesfully post to the database by hardcoding ID

--- handleMongo.py
+++ handleMongo.py
@@ -1,12 +1,15 @@
 import pymongo 
+import certifi
+
+ca = certifi.where()
 
 def db_connect(pw, vid):
-  cluster = pymongo.MongoClient("mongodb+srv://zeroViews:"+pw+"@ytcrawler.0jhwpkg.mongodb.net/?retryWrites=true&w=majority")
+  cluster = pymongo.MongoClient("mongodb+srv://zeroViews:"+pw+"@ytcrawler.0jhwpkg.mongodb.net/?retryWrites=true&w=majority", tlsCAFile = ca)
   db = cluster['python_crawler']
   collection = db['unwatched_id']
 
   def dbPost():
-    return collection.insert_one({"yt_id": vid.id}).inserted_id()
+    return collection.insert_one({"_id": 1, "yt_id": vid.id})
 
   if vid.statistics.viewCount == '0':
     dbPost()
