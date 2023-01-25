5afdb707271fd58aa73b220b2d00ef7ae24ab036
ford-jones <fordnicholasjones@gmail.com>
2023-01-25 22:46:47 +1300

    successfully post to the database with dynamic id

--- handleMongo.py
+++ handleMongo.py
@@ -4,16 +4,17 @@ import certifi
 ca = certifi.where()
 
 def db_connect(pw, vid):
+  
   cluster = pymongo.MongoClient("mongodb+srv://zeroViews:"+pw+"@ytcrawler.0jhwpkg.mongodb.net/?retryWrites=true&w=majority", tlsCAFile = ca)
   db = cluster['python_crawler']
   collection = db['unwatched_id']
 
   def dbPost():
-    return collection.insert_one({"_id": 1, "yt_id": vid.id})
+    return collection.insert_one({"yt_id": vid.id})
 
   if vid.statistics.viewCount == '0':
     dbPost()
-    print(vid.id, ' posted!')
+    print('video ID ', vid.id, ' posted!')
   else:
     print('nothing posted.')
   
