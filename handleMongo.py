2b44c9a2d9d37fcd502c71782ff4ef76f4a3e30d
ford-jones <fordnicholasjones@gmail.com>
2024-07-11 16:08:31 +1200

    Update: Migrate to new endpoint

--- handleMongo.py
+++ handleMongo.py
@@ -4,14 +4,14 @@ import certifi
 ca = certifi.where()
 
 def db_connect(usr, pw, db, cltn, vid):
-  cluster = pymongo.MongoClient("mongodb+srv://"+usr+":"+pw+"@ytcrawler.0jhwpkg.mongodb.net/?retryWrites=true&w=majority", tlsCAFile = ca)
+  cluster = pymongo.MongoClient("mongodb+srv://"+usr+":"+pw+"@afternewmedia.uguszd8.mongodb.net/?retryWrites=true&w=majority", tlsCAFile = ca)
   database = cluster[db]
   collection = database[cltn]
 
   def db_post():
     return collection.insert_one({"yt_id": vid.id})
 
-  if vid.statistics.viewCount == '0':
+  if int(vid.statistics.viewCount) == 0:
     db_post()
     print('video ID ', vid.id, ' posted!')
   else:
